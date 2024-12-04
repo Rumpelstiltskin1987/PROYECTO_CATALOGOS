f62562ee4bb7ea7f0900a36918af113d5d9995fd
ORTEGA CABRERA Enrique J. ALLWARE <EJORTECABRERA@suppliers.tenaris.com>
2024-12-04 11:04:12 -0600

    Agregar archivos de proyecto.

---
+++ DataAccess/MySQLiteContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class MySQLiteContext : DbContext
+    {
+        public MySQLiteContext(DbContextOptions<MySQLiteContext> options) 
+            : base(options) 
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ DataAccess/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Product
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }   
+        public string UrlImage { get; set; }
+
+    }
+
+    public class ProductList
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
